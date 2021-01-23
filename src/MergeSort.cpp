22cc653e42b4c8d4dd64635462aeeb3bf55e4a40
Markamadeos <julipaha@gmail.com>
2021-01-23 23:24:06 +0300

    Add reverse merge sort and tests

--- src/MergeSort.cpp
+++ src/MergeSort.cpp
@@ -1,18 +1,16 @@
 //
 // Created by Ded on 23.01.2021.
 //
-#include <iostream>
 
 using namespace std;
-int n = 0; // Счетчик количества элементов
-const int nmax = 100;
+const int MAX = 100;
 
 void Merg(int *arr, int begin, int end) {
     int i = begin,
             t = 0,
             mid = begin + (end - begin) / 2,
             j = mid + 1,
-            d[nmax];
+            d[MAX];
     while (i <= mid && j <= end) {
 
         if (arr[i] <= arr[j]) {
@@ -59,5 +57,56 @@ void msort(int *arr, int arr_len) {
     MergSort(arr, 0, arr_len - 1);
 }
 
+/// reverse sort
 
+void rMerg(int *arr, int begin, int end) {
+    int i = begin,
+            t = 0,
+            mid = begin + (end - begin) / 2,
+            j = mid + 1,
+            d[MAX];
+    while (i <= mid && j <= end) {
 
+        if (arr[i] >= arr[j]) {
+            d[t] = arr[i];
+            i++;
+        } else {
+            d[t] = arr[j];
+            j++;
+        }
+        t++;
+    }
+    while (i <= mid) {
+        d[t] = arr[i];
+        i++;
+        t++;
+    }
+    while (j <= end) {
+        d[t] = arr[j];
+        j++;
+        t++;
+    }
+    for (i = 0; i < t; i++) {
+        arr[begin + i] = d[i];
+    }
+}
+
+void rMergSort(int *arr, int left, int right) {
+    int temp;
+    if (left < right)
+        if (right - left == 1) {
+            if (arr[right] > arr[left]) {
+                temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        } else {
+            rMergSort(arr, left, left + (right - left) / 2);
+            rMergSort(arr, left + (right - left) / 2 + 1, right);
+            rMerg(arr, left, right);
+        }
+}
+
+void reverseMSort(int *arr, int arr_len) {
+    rMergSort(arr, 0, arr_len - 1);
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -111,6 +111,92 @@ TEST(MergeSort, SortWhithAllNegative) {
     ASSERT_TRUE(isEqual);
 }
 
+TEST(ReverseMergeSort, SortWhithMinValue) {
+
+    // Arragne
+    int n = 10;
+    int *sourceArray = new int[n]{7, 6, 5, 5, 3, 12, 0, 0, 1, INT_MIN};
+    int *expectedArray = new int[n]{12, 7, 6, 5, 5, 3, 1, 0, 0, INT_MIN};
+
+    // Act
+    reverseMSort(sourceArray, n);
+    bool isEqual = 1;
+    for (size_t i = 0; i < n; i++)
+        if (sourceArray[i] != expectedArray[i])
+            isEqual = 0;
+    delete[] sourceArray;
+    delete[] expectedArray;
+
+    // Assert
+    ASSERT_TRUE(isEqual);
+}
+
+TEST(ReverseMergeSort, SortWhithAllNegative) {
+
+    // Arrange
+    int n = 10;
+    int* sourceArray = new int[n] {-1, -1, -1, -1,
+                                   -1, -1, -1, -1, -1, -1};
+    int* expectedArray = new int[n] {-1, -1, -1, -1,
+                                     -1, -1, -1, -1, -1, -1};
+
+    // Act
+    reverseMSort(sourceArray, n);
+    bool isEqual = 1;
+    for (size_t i = 0; i < n; i++)
+        if (sourceArray[i] != expectedArray[i])
+            isEqual = 0;
+    delete[] sourceArray;
+    delete[] expectedArray;
+
+    // Assert
+    ASSERT_TRUE(isEqual);
+}
+
+TEST(ReverseMergeSort, SortWhithAllZero) {
+
+    // Arrange
+    int n = 10;
+    int* sourceArray = new int[n] {0, 0, 0, 0,
+                                   0, 0, 0, 0, 0, 0};
+    int* expectedArray = new int[n] {0, 0, 0, 0,
+                                     0, 0, 0, 0, 0, 0};
+
+    // Act
+    reverseMSort(sourceArray, n);
+    bool isEqual = 1;
+    for (size_t i = 0; i < n; i++)
+        if (sourceArray[i] != expectedArray[i])
+            isEqual = 0;
+    delete[] sourceArray;
+    delete[] expectedArray;
+
+    // Assert
+    ASSERT_TRUE(isEqual);
+}
+
+TEST(ReverseMergeSort, SortWhithMinAndMaxValues) {
+
+    // Arrange
+    int n = 10;
+    int* sourceArray = new int[n] {INT_MIN, INT_MIN, INT_MIN, INT_MIN,
+                                   INT_MIN, INT_MAX, INT_MAX, INT_MAX, INT_MAX, INT_MAX};
+    int* expectedArray = new int[n] {INT_MAX, INT_MAX, INT_MAX, INT_MAX,
+                                     INT_MAX, INT_MIN, INT_MIN, INT_MIN, INT_MIN, INT_MIN};
+
+    // Act
+    reverseMSort(sourceArray, n);
+    bool isEqual = 1;
+    for (size_t i = 0; i < n; i++)
+        if (sourceArray[i] != expectedArray[i])
+            isEqual = 0;
+    delete[] sourceArray;
+    delete[] expectedArray;
+
+    // Assert
+    ASSERT_TRUE(isEqual);
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
